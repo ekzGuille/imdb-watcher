3af6cdb5614b086f4277106d2066b0a3087d7990
Guillermo Sesé Santos <guillermosesesantos@hotmail.com>
2019-12-29 13:38:08 +0100

    extension terminada 👏🏻

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,8 +1,11 @@
 
 // example of using a message handler from the inject scripts
-// chrome.extension.onMessage.addListener(
-//   (request, sender, sendResponse) => {
-//     chrome.pageAction.show(sender.tab.id);
-//     sendResponse();
-//   },
-// );
+chrome.extension.onMessage.addListener(
+  (request, sender, sendResponse) => {
+    // Error handlering
+    if (chrome.pageAction) {
+      chrome.pageAction.show(sender.tab.id);
+      sendResponse();
+    }
+  },
+);
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,47 @@
-chrome.extension.sendMessage({}, function(response) {
-  const readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === 'complete') {
-      clearInterval(readyStateCheckInterval);
+const URLS = [
+  {
+    name: '📺',
+    url: 'https://vidsrc.me/embed/'
+  },
+  {
+    name: '🖥',
+    url: 'https://ww1.putlockers.movie/embed/'
+  }
+];
 
-      // ----------------------------------------------------------
-      // This part of the script triggers when page is done loading
-      console.log('Hello. This message was sent from scripts/inject.js');
-      // ----------------------------------------------------------
+const WORKING_URL = 'https://www.imdb.com/title/';
+const CURRENT_URL = document.location.href;
+const REGEX = new RegExp(WORKING_URL);
+const MOVIE_NAME_ELEMENT = document.querySelector('#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > h1');
 
-    }
-  }, 10);
+const getIMDbID = () => {
+  let id = '';
+  if (REGEX.test(CURRENT_URL)) {
+    id = CURRENT_URL.split(WORKING_URL)[1];
+    id = id.split('/')[0];
+  }
+  return id;
+};
+
+const createUrlWatcher = () => {
+  const MOVIE_ID = getIMDbID();
+  const divWrapper = document.createElement('div');
+  divWrapper.classList.add('subtext');
+  divWrapper.style.textAlign = 'center';
+
+  URLS.forEach((_url, i) => {
+    const a = document.createElement('a');
+    a.href = `${_url.url}${MOVIE_ID}/`;
+    a.target = '__blank';
+    a.innerText = _url.name;
+    a.style.textDecoration = 'none';
+    a.style.margin = '0 10px';
+    a.title = `External link ${i + 1}`;
+    divWrapper.appendChild(a);
+  });
+  MOVIE_NAME_ELEMENT.appendChild(divWrapper);
+};
+
+chrome.extension.sendMessage({}, (response) => {
+  createUrlWatcher();
 });
